7b37d256694f8caf153f1e198b58608c80bcab39
sartikaiy <indriyanisartika10@gmail.com>
2025-12-20 20:31:05 +0700

    Add files via upload

--- products_part3.js
+++ products_part3.js
@@ -2,127 +2,153 @@ const productsPart3 = [
   {
     name: "41. 📓 Note Jurnal Aesthetic",
     category: "home",
-    link: "https://shopee.co.id/Buku-Catatan-Jurnal-aesthetic-A5-Super-Tebal-Diary-360-Halaman-Cover-Kulit-Binder-Anti-Air-Buku-Catatan-Sketsa-Sketchbook-Kerja-Kantor-Bisnis-Harian-Kuliah-Tebal-Cover-Kulit-i.360428779.22845623140",
-    img: "./img/p41.jpg"
+    link: "https://shopee.co.id/Buku-Catatan-Jurnal-aesthetic-A5-Super-Tebal-Diary-360-Halaman-Cover-Kulit-Binder-Anti-Air-Buku-Catatan-Sketsa-Sketchbook-Kerja-Kantor-Bisnis-Harian-Kuliah-Tebal-Cover-Kulit-i.360428779.22845623140"
   },
   {
     name: "42. 👖 Celana Cutbray Stretch",
     category: "fashion",
-    link: "https://shopee.co.id/Flare-Pants-Highwaist-Kulot-Cutbray-Celana-kantor-formal-casual-chinos-stretch-melar-i.138089446.28705809671",
-    img: "./img/p42.jpg"
+    link: "https://shopee.co.id/Flare-Pants-Highwaist-Kulot-Cutbray-Celana-kantor-formal-casual-chinos-stretch-melar-i.138089446.28705809671"
   },
   {
     name: "43. 🛏️ KINTAKUN Sprei Termurah",
     category: "home",
-    link: "https://shopee.co.id/Kintakun-Lite-Sprei-180x200-160x200-Tinggi-20-cm-SP-180-160-KL-TBTSPREI-i.9091181.1318873319",
-    img: "./img/p43.jpg"
+    link: "https://shopee.co.id/Kintakun-Lite-Sprei-180x200-160x200-Tinggi-20-cm-SP-180-160-KL-TBTSPREI-i.9091181.1318873319"
   },
   {
     name: "44. 👟 PVN Teari Sepatu Wanita",
     category: "fashion",
-    link: "https://shopee.co.id/PVN-Taeri-Sepatu-Sneakers-Olahraga-Wanita-Sport-Shoes-Blue-265-i.39910038.12477130498",
-    img: "./img/p44.jpg"
+    link: "https://shopee.co.id/PVN-Taeri-Sepatu-Sneakers-Olahraga-Wanita-Sport-Shoes-Blue-265-i.39910038.12477130498"
   },
   {
     name: "45. 🥤 HAN RIVER Blender",
     category: "home",
-    link: "https://shopee.co.id/HAN-RIVER-Blender-Duo-2in1-Buah-dan-Bumbu-Blender-Jus-Multifungsi-Alat-Peras-Juice-1Liter-Multifungsi-Foodgrade-Blender-Jus-i.300470793.25005717460",
-    img: "./img/p45.jpg"
+    link: "https://shopee.co.id/HAN-RIVER-Blender-Duo-2in1-Buah-dan-Bumbu-Blender-Jus-Multifungsi-Alat-Peras-Juice-1Liter-Multifungsi-Foodgrade-Blender-Jus-i.300470793.25005717460"
   },
   {
     name: "46. 🕌 Mukena Travelling Adem",
     category: "fashion",
-    link: "https://shopee.co.id/FREE-ONGKIR-AZWA-MUKENA-DEWASA-MUKENA-TRAVELING-MINI-POUCH-2IN1-VALEEQA-KATUN-MIKRO-i.387156595.20392787402",
-    img: "./img/p46.jpg"
+    link: "https://shopee.co.id/FREE-ONGKIR-AZWA-MUKENA-DEWASA-MUKENA-TRAVELING-MINI-POUCH-2IN1-VALEEQA-KATUN-MIKRO-i.387156595.20392787402"
   },
   {
     name: "47. 👡 Sandal Luofu Ibu-Ibu",
     category: "fashion",
-    link: "https://shopee.co.id/LUOFU-Sendal-Jelly-Karet-Eva-Wanita-Ban-Dua-Tatak-Hitam-E7199A1-SH4WK-F04--i.554270960.13264778686",
-    img: "./img/p47.jpg"
+    link: "https://shopee.co.id/LUOFU-Sendal-Jelly-Karet-Eva-Wanita-Ban-Dua-Tatak-Hitam-E7199A1-SH4WK-F04--i.554270960.13264778686"
   },
   {
     name: "48. 📱 INFINIX Hot 50",
     category: "tools",
-    link: "https://shopee.co.id/Infinix-Hot-50-8-128GB-Up-to-16GB-Extended-RAM-Helio-G100-6.78-inch-120Hz-FHD-50MP-5000-mAh-NFC-i.342033216.29313594897",
-    img: "./img/p48.jpg"
+    link: "https://shopee.co.id/Infinix-Hot-50-8-128GB-Up-to-16GB-Extended-RAM-Helio-G100-6.78-inch-120Hz-FHD-50MP-5000-mAh-NFC-i.342033216.29313594897"
   },
   {
     name: "49. ☕ Tumblr Termos Premium",
     category: "home",
-    link: "https://shopee.co.id/Termos-Stainless-Premium-Panas-Dingin-12-Jam-1000ML-Minum-Travel-Thermos-Kopi-STAINLESS-316-HIGH-QUALITY-i.28004269.22188460625",
-    img: "./img/p49.jpg"
+    link: "https://shopee.co.id/Termos-Stainless-Premium-Panas-Dingin-12-Jam-1000ML-Minum-Travel-Thermos-Kopi-STAINLESS-316-HIGH-QUALITY-i.28004269.22188460625"
   },
   {
     name: "50. 🪵 Karpet Vinyl Motif Kayu",
     category: "home",
-    link: "https://shopee.co.id/Paus-Biru-Karpet-Lantai-Vinyl-Roll-Tebel-0.7mm-Decorasi-Lantai-50cm-Meteran-2m-KLV-i.521149963.13454501754",
-    img: "./img/p50.jpg"
+    link: "https://shopee.co.id/Paus-Biru-Karpet-Lantai-Vinyl-Roll-Tebel-0.7mm-Decorasi-Lantai-50cm-Meteran-2m-KLV-i.521149963.13454501754"
   },
   {
     name: "51. 🔪 Chopper 50 Ribuan AWET",
     category: "home",
-    link: "https://shopee.co.id/Chopper-Blender-Daging-Penggiling-Meat-Blender-Elektrik-Stainless-i.6547323.4249548306",
-    img: "./img/p51.jpg"
+    link: "https://shopee.co.id/Chopper-Blender-Daging-Penggiling-Meat-Blender-Elektrik-Stainless-i.6547323.4249548306"
   },
   {
     name: "52. 🧥 Sweater Hoodie Wanita",
     category: "fashion",
-    link: "https://shopee.co.id/Parisdivarious-Sweater-Hoodie-Jacket-Zipper-Resleting-Polos-Fleece-Tebal-Size-S-XXL-Pria-Wanita-i.159631626.26063168876",
-    img: "./img/p52.jpg"
+    link: "https://shopee.co.id/Parisdivarious-Sweater-Hoodie-Jacket-Zipper-Resleting-Polos-Fleece-Tebal-Size-S-XXL-Pria-Wanita-i.159631626.26063168876"
   },
   {
     name: "53. 💄 BIOAQUA Cushion",
     category: "beauty",
-    link: "https://s.shopee.co.id/7KpuallKU0",
-    img: "./img/p53.jpg"
+    link: "https://s.shopee.co.id/7KpuallKU0"
   },
   {
     name: "54. 👡 Sandal Flatshoes",
     category: "fashion",
-    link: "https://s.shopee.co.id/2qNVEez6mo",
-    img: "./img/p54.jpg"
+    link: "https://s.shopee.co.id/2qNVEez6mo"
   },
   {
     name: "55. 🧴 Vaseline Handbody 400ml",
     category: "beauty",
-    link: "https://s.shopee.co.id/2g452bK0tA",
-    img: ".img/p55.jpg"
+    link: "https://s.shopee.co.id/2g452bK0tA"
   },
   {
     name: "56. 🌧️ Jas Hujan Anak Motif Kucing",
     category: "fashion",
-    link: "https://s.shopee.co.id/4AsspBVOaV",
-    img: "./img/p56.jpg"
+    link: "https://s.shopee.co.id/4AsspBVOaV"
   },
   {
     name: "57. 🦾 Penyangga Pelurus Siku",
     category: "tools",
-    link: "https://s.shopee.co.id/9zqflyhBQF",
-    img: "./img/p57.jpg"
+    link: "https://s.shopee.co.id/9zqflyhBQF"
   },
   {
     name: "58. ☕ Tumbler Coffee Premium",
     category: "home",
-    link: "https://s.shopee.co.id/AUmwN7i0ki",
-    img: "./img/p58.jpg"
+    link: "https://s.shopee.co.id/AUmwN7i0ki"
   },
   {
     name: "59. 👕 Jennie Kemeja Linen",
     category: "fashion",
-    link: "https://s.shopee.co.id/9fDpNidca4",
-    img: "./img/p59.jpg"
+    link: "https://s.shopee.co.id/9fDpNidca4"
   },
   {
     name: "60. 👜 Hirakiya Sling Bag",
     category: "fashion",
-    link: "https://s.shopee.co.id/VzaSwoIF7",
-    img: "./img/p60.jpg"
+    link: "https://s.shopee.co.id/VzaSwoIF7"
   },
   {
     name: "61. 👖 Alpha Cargo Jeans",
     category: "fashion",
-    link: "https://s.shopee.co.id/8peiOLYHlT",
-    img: "./img/p61.jpg"
+    link: "https://s.shopee.co.id/8peiOLYHlT"
+  },
+
+  /* === TAMBAHAN BARU === */
+  {
+    name: "62. 🩰 Sepatu Ballet",
+    category: "fashion",
+    link: "https://s.shopee.co.id/2g4FCkExUc"
+  },
+  {
+    name: "63. 👡 Bellvania Flatshoes",
+    category: "fashion",
+    link: "https://s.shopee.co.id/2LROoFDOr8"
+  },
+  {
+    name: "64. 👡 Mishell Flatshoes",
+    category: "fashion",
+    link: "https://s.shopee.co.id/9zqpwOICQK"
+  },
+  {
+    name: "65. 👡 Mulls Flatshoes",
+    category: "fashion",
+    link: "https://s.shopee.co.id/7Kq4leJqLJ"
+  },
+  {
+    name: "66. 👡 ISMI Flatshoes",
+    category: "fashion",
+    link: "https://s.shopee.co.id/4q8jn9mw3y"
+  },
+  {
+    name: "67. 👡 ELEORA Flatshoes",
+    category: "fashion",
+    link: "https://s.shopee.co.id/20oYQ10zFS"
+  },
+  {
+    name: "68. 👠 Casandra Heels",
+    category: "fashion",
+    link: "https://s.shopee.co.id/qcb1vQ98K"
+  },
+  {
+    name: "69. 👡 Flatshoes Glossy",
+    category: "fashion",
+    link: "https://s.shopee.co.id/2qNfPhT26U"
+  },
+  {
+    name: "70. 👡 Flatshoes Premium",
+    category: "fashion",
+    link: "https://s.shopee.co.id/2qNfPhT26U"
   }
 ];
